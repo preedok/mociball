6d4cd2ba15244876f22a2e31e22af07d57af3f08
preedok <Edo.vivoy21@gmail.com>
2025-03-10 01:02:51 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,9 +1,9 @@
 export default [
-    { author: "Iqbal Apredo", quotes: "Apa pun yang terjadi, senyummu adalah cahaya dalam hidupku, moci." },
-    { author: "Iqbal Apredo", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram, karena setiap detik bersamamu adalah melodi terindah." },
-    { author: "Iqbal Apredo", quotes: "Hei! Cintaku padamu tak terhingga, UwU." },
-    { author: "Iqbal Apredo", quotes: "Aku mencintaimu lebih dari kata-kata bisa ungkapkan." },
-    { author: "Iqbal Apredo", quotes: "Walau aku kadang ngeselin, cintaku padamu selalu tulus dan tak terbatas." },
-    { author: "Iqbal Apredo", quotes: "Aku mencintaimu, moci, dengan segenap hatiku." },
-    { author: "Iqbal Apredo", quotes: "Maafkan aku yang kadang cemburu, itu hanya karena cintaku padamu begitu dalam." },
+    { author: "Iqbal Apredo", quotes: "Apa pun yang terjadi, senyummu adalah sumber kebahagiaanku, moci. Tetaplah tersenyum, karena itu membuat hariku lebih berarti." },
+    { author: "Iqbal Apredo", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram, setiap nada ini adalah ungkapan rasa yang mendalam untukmu." },
+    { author: "Iqbal Apredo", quotes: "Hei! Cintaku padamu adalah hal terindah yang pernah kutemukan, UwU. Kehadiranmu memberi arti dalam hidupku." },
+    { author: "Iqbal Apredo", quotes: "Aku mencintaimu dengan tulus, lebih dari sekadar kata-kata. Setiap momen bersamamu adalah harta yang tak ternilai." },
+    { author: "Iqbal Apredo", quotes: "Walau kadang aku bisa membuatmu kesal, percayalah, cintaku padamu adalah komitmen yang selalu kupegang." },
+    { author: "Iqbal Apredo", quotes: "Aku mencintaimu, moci, dengan sepenuh hati. Setiap detik bersamamu adalah kenangan yang ingin kuabadikan." },
+    { author: "Iqbal Apredo", quotes: "Maafkan aku yang kadang cemburu, itu hanya karena aku sangat menghargai kehadiranmu dalam hidupku." },
 ];
