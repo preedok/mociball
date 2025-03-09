48c16497a8cc2fd8c34ba479db6c1f9fe68b6c54
preedok <Edo.vivoy21@gmail.com>
2025-03-10 01:01:14 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,9 +1,9 @@
 export default [
-    { author: "Iqbal Apredo", quotes: "Apa pun yang terjadi, tetaplah tersenyum ya moci" },
-    { author: "Iqbal Apredo", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "Iqbal Apredo", quotes: "Hei! Aku mencintaimu! UwU" },
-    { author: "Iqbal Apredo", quotes: "Aku mencintaimu." },
-    { author: "Iqbal Apredo", quotes: "Aku mungkin ngeselin setiap hari, tetapi aku mencintaimu setiap hari." },
-    { author: "Iqbal Apredo", quotes: "Aku mencintaimu moci" },
-    { author: "Iqbal Apredo", quotes: "Aku minta maaf orangnya cemburuan, suka curiga an dan bucin parah... karena aku mencintaimu." },
+    { author: "Iqbal Apredo", quotes: "Apa pun yang terjadi, senyummu adalah cahaya dalam hidupku, moci." },
+    { author: "Iqbal Apredo", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram, karena setiap detik bersamamu adalah melodi terindah." },
+    { author: "Iqbal Apredo", quotes: "Hei! Cintaku padamu tak terhingga, UwU." },
+    { author: "Iqbal Apredo", quotes: "Aku mencintaimu lebih dari kata-kata bisa ungkapkan." },
+    { author: "Iqbal Apredo", quotes: "Walau aku kadang ngeselin, cintaku padamu selalu tulus dan tak terbatas." },
+    { author: "Iqbal Apredo", quotes: "Aku mencintaimu, moci, dengan segenap hatiku." },
+    { author: "Iqbal Apredo", quotes: "Maafkan aku yang kadang cemburu, itu hanya karena cintaku padamu begitu dalam." },
 ];
